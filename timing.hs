3e075c7c7f14e82e9fb312d6d7d6098ffe493252
Kia <2592730+superbaud@users.noreply.github.com>
2016-01-27 20:10:41 -0800

    timing.hs

---
+++ timing.hs
@@ -0,0 +1,30 @@
+import Control.Exception
+import Control.Concurrent
+import System.Process 
+import System.IO
+import Data.Maybe
+import System.Clock
+
+main =  createProcess (proc "./pass" []){ std_in=CreatePipe, std_out=CreatePipe} >>= \(i, j, k, h) -> (timeRun (fromJust i) (fromJust j) 10000)
+
+
+timeRun sin sout test = do
+    hGetLine sout                                   -- first thing, please kill l8r and strip off first line earlier
+    hPutStrLn sin $ show test
+    start <- getTime Monotonic
+    hGetLine sout
+    hGetLine sout
+    end <- getTime Monotonic
+    fuck <- hGetLine sout                           -- remove me i'm just the runt end of "password"K
+    
+    putStrLn $ "fuck you " ++ fuck
+    let diff = diffTimeSpec start end
+    putStrLn (show diff)
+    return diff
+
+
+
+digitsToInt digits = foldl' add 0 where add acc d = 10 * acc + d
+
+makeGuess known unknown full = 
+
